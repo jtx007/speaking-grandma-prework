93c0a9a57c1b4094b2fe51d8ae1beef7016ad44d
James Thomas <jamesjacobthomas7@gmail.com>
2018-04-26 19:58:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    'I LOVE YOU TOO PUMPKIN'
+    'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == phrase.upcase
     'NO NOT SINCE 1938'
   else
-    'HUH? SPEAK UP, SONNY!'
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
